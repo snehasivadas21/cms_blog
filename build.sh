a93f56cc31374d13fe62d05be11a6d9be7219e08
snehasivadas21 <snehasivadas2002@gmail.com>
2026-06-10 20:55:20 +0530

    fix: superuser creation in build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,12 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 
 python manage.py migrate
+
+python manage.py shell -c "
+import os
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'ssneha2103@gmail.com', os.environ.get('SUPERUSER_PASSWORD'))
+print('Superuser ready')
+"
